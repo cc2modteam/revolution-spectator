107e68b1f8deddbd39fc9890b35da1d80f1d5d33
inb <inorton@gmail.com>
2024-12-11 00:22:05 +0000

    Mention 1.3+

--- content/scripts/library_custom_3.lua
+++ content/scripts/library_custom_3.lua
@@ -5,7 +5,7 @@ g_revolution_welcome = "Spectator Console"
 if g_radar_ranges == nil then
 	-- revolution is not installed, tell the user
 	update_screen_overrides = function(screen_w, screen_h, ticks)
-		update_ui_text(20, 20, "Spectator mode needs Revolution mod 1.3 ",
+		update_ui_text(20, 20, "Spectator mode needs Revolution mod 1.3+",
 				110, 0, color_white, 0)
 		return false
 	end
